d3fe7ccf8628fa8c8e50f94037b098acae3ba9d9
teddykolios <tkolios@wustl.edu>
2021-04-14 01:18:37 -0500

    menu class done i think

--- src/cse237/Menu.java
+++ src/cse237/Menu.java
@@ -1,5 +1,32 @@
 package cse237;
 
-public class Menu {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Menu {
+	
+	private ArrayList<String> message;
+	private int options;
+	private Scanner keyBoardIn;
+	
+	public Menu(int numOptions, ArrayList<String> messages) {
+		this.message = messages;
+		this.options = numOptions;
+		this.keyBoardIn = new Scanner(System.in);
+	}
+	
+	private void displayMessage() {
+		for(int i = 0; i < this.message.size(); ++i) {
+			System.out.println(message.get(i));
+		}
+	}
+	private int getInput() {
+		int selected = keyBoardIn.nextInt();
+		while(selected < 1 || selected > this.options) {
+			System.out.println("Please enter a valid option:");
+			selected = keyBoardIn.nextInt();
+		}
+		return selected;
+	}
+	
 }
