cd4e38566aec7bdfb28ee8d5603fbf510e6a82b7
teddykolios <tkolios@wustl.edu>
2021-04-28 13:39:22 -0500

    fixed state message bug

--- src/cse237/StatesList.java
+++ src/cse237/StatesList.java
@@ -59,57 +59,7 @@ public class StatesList {
 		states.add("WV");
 		states.add("WI");
 		states.add("WY");
-		
-//		states.add("alabama");
-//		states.add("alaska");
-//		states.add("arizona");
-//		states.add("arkansas");
-//		states.add("california");
-//		states.add("colorado");
-//		states.add("connecticut");
-//		states.add("delaware");
-//		states.add("florida");
-//		states.add("georgia");
-//		states.add("hawaii");
-//		states.add("idaho");
-//		states.add("illinois");
-//		states.add("indiana");
-//		states.add("iowa");
-//		states.add("kansas");
-//		states.add("kentucky");
-//		states.add("louisiana");
-//		states.add("maine");
-//		states.add("maryland");
-//		states.add("massachusetts");
-//		states.add("michigan");
-//		states.add("minnesota");
-//		states.add("mississippi");
-//		states.add("missouri");
-//		states.add("montana");
-//		states.add("nebraska");
-//		states.add("nevada");
-//		states.add("new hampshire");
-//		states.add("new jersey");
-//		states.add("new mexico");
-//		states.add("new york");
-//		states.add("north carolina");
-//		states.add("north dakota");
-//		states.add("ohio");
-//		states.add("oklahoma");
-//		states.add("oregon");
-//		states.add("pennsylvania");
-//		states.add("rhode island");
-//		states.add("south carolina");
-//		states.add("south dakota");
-//		states.add("tennessee");
-//		states.add("texas");
-//		states.add("utah");
-//		states.add("vermont");
-//		states.add("virginia");
-//		states.add("washington");
-//		states.add("west virginia");
-//		states.add("wisconsin");
-//		states.add("wyoming");
+
 	}
 	
 	public boolean isState(String s) {
